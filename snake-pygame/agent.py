1df3129ed5d83b059f93039cddd5a90bfae8223f
wasi-coder <wasialsafa025@gmail.com>
2026-09-21 21:29:40 +0600

    took a previously built snake game and updated it so now it can be played automatically by a agent no need for keyboard inputs and also adde the state values and reward functions needed

---
+++ snake-pygame/agent.py
@@ -0,0 +1,16 @@
+import torch
+import random 
+import numpy as no 
+from game import SnakeGameAI, Direction,Point
+from collections import deque
+
+
+MAX_MEMORY =100_000
+BATCH_SIZE =1000
+
+LR =0.001
+
+class Agent:
+    def __init__(self):
+        pass
+
