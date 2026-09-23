c1530d82fd5f90007d5b9548d774a5789289c945
wasi-coder <wasialsafa025@gmail.com>
2026-09-23 15:57:11 +0600

    working on the agent

--- snake-pygame/agent.py
+++ snake-pygame/agent.py
@@ -12,5 +12,98 @@ LR =0.001
 
 class Agent:
     def __init__(self):
+        self.n_games=0
+        self.epsilon =0 # parameter to control the randomness 
+        self.gamma =0 #discount factor 
+        self.memory =deque(maxlen=MAX_MEMORY) #popleft()
+        #todo: model,trainer
+
+
+
+    def get_state(self,game):
+
+        head = game.snake[0]
+        point_l =Point(head.x - 20, head.y)
+        point_r =Point(head.x + 20, head.y)
+        point_u =Point(head.x, head.y - 20)
+        point_d =Point(head.x, head.y + 20)
+
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        state=[
+
+            (dir_r and game.is_collision(point_r))or
+            (dir_l and game.is_collision(point_l))or
+            (dir_u and game.is_collision(point_u))or
+            (dir_d and game.is_collision(point_d)),
+
+             
+                                                
+
+        ]
+
+
+
+        
+    def remember (self,state,action,reward, next_state, done):
         pass
 
+    def train_long_memory(self):
+        pass
+
+    def train_short_memory(self,state,action,reward, next_state, done):
+        pass
+
+    def get_action (self,state):
+        pass
+
+def train():
+    plot_scores=[]
+    plot_mean_scores=[]
+    total_scores =0
+    record= 0
+    agent =Agent()
+    game= SnakeGameAI()
+    while True:
+        #get old state
+        state_old=agent.get_state(game)
+
+        #get mode
+        final_move =agent.get_action(state_old)
+
+        #perform move and get new state
+
+        reward, done, score = game.play_step(final_move)
+        state_new =agent.get_state(game)
+
+
+        #train the short memeory
+
+        agent.train_short_memory(state_old,final_move,reward,state_new,done)
+
+        #remember
+
+        agent.remember(state_old,final_move,reward,state_new,done)
+
+
+        if done:
+            #train the long mem or replay mem 
+            #plot reasult
+            game.reset()
+            agent.n_games+=1
+            agent.train_long_memory()
+
+            if score >record:
+                record =score
+                #agent.model.save()
+
+            print('game',agent.n_games,'score',score, 'record',record)
+
+            #todo: plot
+
+
+if __name__ == '__main__':
+    train()
